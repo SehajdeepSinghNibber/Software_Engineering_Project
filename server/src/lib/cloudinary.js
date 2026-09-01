18eb6cee0f05e59342f0dd5443f4514ab6b5810c
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-09-01 10:00:44 +0530

    Auth added

---
+++ server/src/lib/cloudinary.js
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from "cloudinary"
+import { config } from "dotenv"
+
+config();
+
+cloudinary.config({
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+})
+
+export default cloudinary;
